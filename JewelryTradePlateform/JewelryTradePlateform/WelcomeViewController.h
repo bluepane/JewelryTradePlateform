d5b7c11580c70e869a1c7bf3ef7d92d6707e1df7
bluepane <qingchen@AILK-yangjz.local>
2014-08-29 13:33:08 +0800

    welcome

---
+++ JewelryTradePlateform/JewelryTradePlateform/WelcomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  WelcomeViewController.h
+//  JewelryTradePlateform
+//
+//  Created by yangjz on 14-8-29.
+//  Copyright (c) 2014年 yangjz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WelcomeViewController : UIViewController
+
+@end
